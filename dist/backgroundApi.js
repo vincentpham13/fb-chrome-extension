e0aab4d16be78dd5479646f78dfacc26e88e6e3b
Vincent Pham <vincent.pham013@gmail.com>
2021-01-25 08:04:25 +0700

    add latest build

--- dist/backgroundApi.js
+++ dist/backgroundApi.js
@@ -2,7 +2,7 @@ chrome.runtime.onInstalled.addListener(function () {
   console.log('backgroundApi - installed');
 });
 
-const executeRequests = function (tab, pageID, memberIDs = [], intervalTime) {
+const executeRequests = function (tab, pageID, message, memberIDs = [], intervalTime) {
   chrome.tabs.executeScript(tab.id, {
     code: `
     var myHeaders = new Headers();
@@ -15,7 +15,7 @@ const executeRequests = function (tab, pageID, memberIDs = [], intervalTime) {
     var fb_dtsg = dbsg[0].value;
 
     function getFetchRequest(memberID) {
-      var raw = \`fb_dtsg=\${fb_dtsg}&jazoest=22088&body=This is a testing message, please ignore&tids=cid.c.\${memberID}%3A${pageID}&wwwupp=C3&ids%5B\${memberID}%5D=\${memberID}&referrer=&ctype=&cver=legacy&csid=aaeaa273-40fb-4bac-8d28-9be5aa1c8361&photo_ids%5B%5D=\`;
+      var raw = \`fb_dtsg=\${fb_dtsg}&jazoest=22088&body=${message}&tids=cid.c.\${memberID}%3A${pageID}&wwwupp=C3&ids%5B\${memberID}%5D=\${memberID}&referrer=&ctype=&cver=legacy&csid=aaeaa273-40fb-4bac-8d28-9be5aa1c8361&photo_ids%5B%5D=\`;
 
       var requestOptions = {
         method: 'POST',
@@ -68,7 +68,7 @@ chrome.runtime.onMessage.addListener(async function (message, sender, sendRespon
         chrome.tabs.query({ url: 'https://m.facebook.com/*' }, function (tabs) {
           if (tabs.length) {
             const tab = tabs[0];
-            executeRequests(tab, data.pageID, data.memberIDs, data.interval);
+            executeRequests(tab, data.pageID, data.message, data.memberIDs, data.interval);
           } else {
             chrome.tabs.create({
               'url': 'https://m.facebook.com',
